24fe259889d63925ac0338b42eb1f9039e4c12f1
son1258 <dtswork88@gmail.com>
2024-05-11 14:48:18 +0700

    upload

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Social = () => {
+    return(
+        <div className="home__social">
+            <a href="https://www.instagram.com/d.thanhson.68/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-instagram-alt"></i>
+            </a>
+            <a href="https://github.com/son1258" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-github"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100028876574423" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-facebook-f"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/son-do-693039280/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-linkedin"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
